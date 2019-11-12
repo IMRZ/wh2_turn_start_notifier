feccc5821944dde4021f23503f8a2c9ede5354fb
IMRZ <im.razzak@gmail.com>
2019-11-12 12:07:09 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private MemoryReader memoryReader;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private void Start()
+        {
+            memoryReader = new MemoryReader();
+
+            int CURRENT_TURN = memoryReader.getCurrentTurnNumber();
+
+            MethodInvoker inv = delegate
+            {
+                int currentTurnNumber = memoryReader.getCurrentTurnNumber();
+                bool isIngameMenuActive = memoryReader.isIngameMenuActive();
+                bool isActive = memoryReader.isWh2ProcessWindowActive();
+
+                if (currentTurnNumber > CURRENT_TURN)
+                {
+                    Debug.WriteLine("[WH2_TURN_START_NOTIFIER] new turn started");
+                    CURRENT_TURN = currentTurnNumber;
+                    if (!isActive) System.Media.SystemSounds.Beep.Play();
+                }
+                else if (currentTurnNumber == CURRENT_TURN && isIngameMenuActive)
+                {
+                    Debug.WriteLine("[WH2_TURN_START_NOTIFIER] action is required!");
+                    if (!isActive) System.Media.SystemSounds.Asterisk.Play();
+                }
+
+                this.label1.Text = "current turn number: " + currentTurnNumber;
+                this.label2.Text = "player action required: " + isIngameMenuActive;
+            };
+
+            Task.Run(() => {
+                while (true)
+                {
+                    try
+                    {
+                        this.Invoke(inv);
+                        System.Threading.Thread.Sleep(1000);
+                    } catch
+                    {
+                        // do nothing, application is stopping
+                    }
+                }
+            });
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            memoryReader.resetAddressCurrentTurnNumber();
+        }
+    }
+}
---
+++ WindowsFormsApp1/MemoryReader.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    class MemoryReader
+    {
+        const int PROCESS_VM_READ = 0x0010;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32")]
+        public static extern int ReadProcessMemory(IntPtr hProcess, IntPtr lpBase, ref long lpBuffer, int nSize, int lpNumberOfBytesRead);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetWindowThreadProcessId(IntPtr handle, out IntPtr processId);
+
+        // offsets for: current turn number
+        const int OFFSET_CTN_1 = 0x031AA488;
+        const int OFFSET_CTN_2 = 0x0;
+        const int OFFSET_CTN_3 = 0xFC0;
+
+        // offsets for: ingame menu/window active (not sure what exactly, but it works)
+        const int OFFSET_IMA_1 = 0x3486FDC;
+
+        // wh2 process variables
+        private Process wh2Process;
+        private IntPtr wh2ProcessBaseAddress = IntPtr.Zero;
+        private IntPtr wh2ProcessHandle = IntPtr.Zero;
+
+        // addresses to scan
+        private IntPtr addressCurrentTurnNumber = IntPtr.Zero;
+        private IntPtr addressIngameMenuActive = IntPtr.Zero;
+
+        public MemoryReader()
+        {
+            initProcess();
+
+            addressCurrentTurnNumber = GetCurrentTurnNumberAddress();
+            addressIngameMenuActive = wh2ProcessBaseAddress + OFFSET_IMA_1;
+        }
+
+        private void initProcess()
+        {
+            wh2Process = Process.GetProcessesByName("Warhammer2")[0];
+
+            foreach (ProcessModule m in wh2Process.Modules)
+            {
+                if (m.ModuleName.Contains("Warhammer2"))
+                {
+                    wh2ProcessBaseAddress = m.BaseAddress;
+                    break;
+                }
+            }
+
+            if (wh2ProcessBaseAddress == IntPtr.Zero)
+            {
+                throw new Exception("Warhammer2 process not found!");
+            }
+
+            wh2ProcessHandle = OpenProcess(PROCESS_VM_READ, false, wh2Process.Id);
+        }
+
+        private IntPtr GetCurrentTurnNumberAddress()
+        {
+            long readValue = 0;
+            ReadProcessMemory(wh2ProcessHandle, (wh2ProcessBaseAddress + OFFSET_CTN_1), ref readValue, sizeof(long), 0);
+            ReadProcessMemory(wh2ProcessHandle, ((IntPtr) readValue + OFFSET_CTN_2), ref readValue, sizeof(long), 0);
+            return (IntPtr) readValue + OFFSET_CTN_3;
+        }
+
+        public void resetAddressCurrentTurnNumber()
+        {
+            addressCurrentTurnNumber = GetCurrentTurnNumberAddress();
+        }
+
+        public int getCurrentTurnNumber()
+        {
+            long readValue = 0;
+            ReadProcessMemory(wh2ProcessHandle, addressCurrentTurnNumber, ref readValue, sizeof(int), 0);
+            return (int) readValue;
+        }
+
+        public bool isIngameMenuActive()
+        {
+            long readValue = 0;
+            ReadProcessMemory(wh2ProcessHandle, addressIngameMenuActive, ref readValue, sizeof(byte), 0);
+            return (readValue == 1);
+        }
+
+        public bool isWh2ProcessWindowActive()
+        {
+            var activatedHandle = GetForegroundWindow();
+            if (activatedHandle == IntPtr.Zero)
+            {
+                return false;
+            }
+
+            IntPtr activeProcId;
+            GetWindowThreadProcessId(activatedHandle, out activeProcId);
+
+            return Process.GetProcessById((int) activeProcId).ProcessName == "Warhammer2";
+        }
+    }
+}
